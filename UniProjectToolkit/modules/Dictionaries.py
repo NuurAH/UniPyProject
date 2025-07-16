9a16b2b6fd78d348e48661e196c98acac4f51ee8
NuurAH <you@example.com>
2025-07-16 14:47:15 +0000

    Some more work uploaded, unsure whether to redo reverse complement

---
+++ UniProjectToolkit/modules/Dictionaries.py
@@ -0,0 +1,29 @@
+complement_table = {
+    "A" : "T",
+    "T" : "A",
+    "G" : "C",
+    "C" : "G"
+}
+codon_table = {
+    "F" : ["TTT","TTC"],
+    "L" : ["TTA", "TTG", "CTT", "CTA", "CTG" ],
+    "I" : ["ATT", "ATC", "ATA"],
+    "M" : ["ATG"],
+    "V" : ["GTT", "GTC", "GTG", "GTA"],
+    "S" : ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
+    "P" : ["CCT", "CCC", "CCA", "CCG"],
+    "T" : ["ACT", "ACC", "ACA", "ACG"],
+    "A" : ["GCT", "GCC", "GCA", "GCG"],
+    "Y" : ["TAT", "TAC"],
+    "*" : ["TAA", "TAG", "TGA"],
+    "H" : ["CAT", "CAC"],
+    "Q" : ["CAA", "CAG"],
+    "N" : ["AAT", "AAC"],
+    "K" : ["AAA", "AAG"],
+    "D" : ["GAT", "GAC"],
+    "E" : ["GAA", "GAG"],
+    "C" : ["TGT", "TGC"],
+    "W" : ["TGG"],
+    "R" : ["CGT", "CGC", "CGA", "CGG", "AGA", "AGG" ],
+    "G" : ["GGT", "GGC", "GGA", "GGG" ]
+}
---
+++ UniProjectToolkit/modules/Reverse_Complement.py
@@ -0,0 +1,41 @@
+from Dictionaries import complement_table
+#reverse complement manipulations
+
+def reverse_complement(nucleotide):
+    if nucleotide in complement_table:
+        return nucleotide
+    else:
+        print("This is not a base" + " " + nucleotide)
+        return " "
+
+#remade the while loop so that it is more efficient, went from outputting 8x the wrong value (dummy nucleotides not in the dictionary) to outputting the exact amount
+def reversing(start, dna, dna_list):
+    dna_length = len(dna)
+    while start < dna_length:
+        dna_list.append(dna[start])
+        complement = reverse_complement(dna_list[-1])
+        for key, value in complement_table.items():
+            if complement in value :
+                complement_list.append(key)
+        start+=1
+        if start == dna_length:
+            return dna_list, complement_list
+    #Reversing the string
+
+def reverse_print (complement_output, complement_list):   
+    for base in complement_list:
+        complement_output += base
+        complement_output = complement_output[::-1]
+    print(complement_output)
+    return complement_output
+
+if __name__ == "__main__":
+    dna = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGXGGGTTTCTCAGATAACTGGGCCXCCTGCGCTCAGGAGGCCTTCACCCTCXTGCTCTGGGTAAAGTTCAXTTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTGAT"
+    start = 0
+    dna_list = []
+    complement_list = []
+    complement = 0
+    complement_output = ""
+
+    reversing(start, dna, dna_list)
+    reverse_print(complement_output, complement_list)
